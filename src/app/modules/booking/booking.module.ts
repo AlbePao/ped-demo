853601f7ff6b92577cc8b3e01393dd85972f064f
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-05-08 17:03:30 +0200

    Add missing routing modules

--- src/app/modules/booking/booking.module.ts
+++ src/app/modules/booking/booking.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
+import { BookingRoutingModule } from './booking-routing.module';
 import { BookingComponent } from './booking.component';
 
 @NgModule({
   declarations: [BookingComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, BookingRoutingModule, SharedModule],
 })
 export class BookingModule {}
