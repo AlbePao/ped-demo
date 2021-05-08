71bb4db2c8197de57638018b630ebdd37d1f5e21
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-05-08 15:00:56 +0200

    Better routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule),
   },
   {
     path: 'booking',
     loadChildren: () => import('./modules/booking/booking.module').then((m) => m.BookingModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
   },
 ];
 
