5b9521c936a0219069264a982ec816a36d646f8a
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-05-09 23:02:24 +0200

    Home view

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivitiesService } from '@app/core/services/activities.service';
+import { AnnouncementsService } from '@app/core/services/announcements.service';
+import { NewsService } from '@app/core/services/news.service';
+import { map, share, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent {
+  activities$ = this.activitesService.getUserActivities().pipe(share());
+  announcements$ = this.announcementsService.getAnnouncements();
+  news$ = this.newsService.getNews();
 
-  ngOnInit(): void {}
+  nextActivities$ = this.activities$.pipe(
+    map((activities) => activities.filter((activity) => activity.status === 'accepted')),
+  );
+
+  pendingActivities$ = this.activities$.pipe(
+    map((activities) => activities.filter((activity) => activity.status === 'pending')),
+    shareReplay(),
+  );
+
+  constructor(
+    private readonly activitesService: ActivitiesService,
+    private readonly announcementsService: AnnouncementsService,
+    private readonly newsService: NewsService,
+  ) {}
+
+  cancelBooking(activityId: number): void {
+    console.log(`Deleted activity with id ${activityId}`);
+  }
 }
